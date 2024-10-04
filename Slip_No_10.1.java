275558df39a39f588bb8dee4d34e688f60934a85
Om Dhormare <143721889+omdhormare@users.noreply.github.com>
2024-10-04 19:31:17 +0530

    Add files via upload

---
+++ Slip_No_10.1.java
@@ -0,0 +1,30 @@
+/*Write a java program to count the frequency of each character in a given string. */
+import java.util.*;
+class Frequency 
+{
+    public static void main(String[] args) 
+    {
+        String s1;
+        char ch;
+        int cnt;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String : ");
+        s1 = sc.nextLine();
+        s1 = s1.toLowerCase(); 
+
+        for (ch = 'a'; ch <= 'z'; ch++) 
+        {
+            cnt = 0;
+            for (int i = 0; i < s1.length(); i++) 
+            {
+                if (ch == s1.charAt(i)) 
+                {
+                    cnt++;
+                }
+            }
+            if (cnt != 0) 
+                System.out.println(ch + "\t" + cnt);
+        }
+    }
+}
