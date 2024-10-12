551a98650a384e82ff1df832d0bda03b3d65a986
Om Dhormare <143721889+omdhormare@users.noreply.github.com>
2024-10-12 19:14:04 +0530

    Add files via upload

---
+++ Slip_No_14.1.java
@@ -0,0 +1,25 @@
+/* Write a Java program to calculate power of a number using recursion */
+import java.util.*;
+class rec
+{
+    public static void main(String[] args) 
+    {
+        int n,p,res;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        n=sc.nextInt();
+        System.out.print("Enter Number Power: ");
+        p=sc.nextInt();
+        
+        res=power(n,p);
+
+        System.out.print("Power Of Number : "+res);
+    }
+    static int power(int n,int p)
+    {
+        if(p==0)
+          return 1;
+        else
+          return n * power(n,p-1);
+    }
+}
